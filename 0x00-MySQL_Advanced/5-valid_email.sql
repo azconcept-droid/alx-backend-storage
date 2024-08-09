c8ef39cb30756f4c978ca7957648d6638a94756a
Yahaya Azeez Adebayo <azconcept2016@gmail.com>
2024-08-09 23:59:50 +0100

    add some queries

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -2,4 +2,11 @@
 -- only when the email has been changed.
 delimiter |;
 
+CREATE TRIGGER reset_attri AFTER INSERT ON orders
+  FOR EACH ROW
+  BEGIN
+    UPDATE items SET quantity = quantity - NEW.number WHERE name = NEW.item_name;
+  END;
+|
+
 delimiter ;|
