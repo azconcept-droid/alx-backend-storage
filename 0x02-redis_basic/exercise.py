9c2f25107c99ba2d228e6217aeb47b1867e51526
Yahaya Azeez <azconcept2016@gmail.com>
2024-08-14 22:01:30 +0100

    stores data in Redis under the generated key

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,3 +1,23 @@
 #!/usr/bin/env python3
-""" Redis exercide module
+""" Redis exercise module
 """
+import redis
+import uuid
+from typing import Any
+
+
+class Cache:
+    """ Cache class
+    """
+
+    def __init__(self):
+        """Initialize Cache class"""
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: Any) -> str:
+        """Store method taht store key in cache"""
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+
+        return key
